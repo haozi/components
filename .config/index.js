b1347d7ec4e055736f3f40d63bdf2cb50e500055
haozime <syntaxright@gmail.com>
2016-12-07 10:03:10 +0800

    增加桌面浏览器autoprefixer 规则

--- .config/index.js
+++ .config/index.js
@@ -8,6 +8,6 @@ module.exports = {
   output_css: './dist/index.css',
   output_css_min: './dist/index.min.css',
   autoprefixer: {
-    browsers: ['Android >= 4', 'iOS >=7', 'IE >= 9', 'Firefox >= 50', 'Chrome >= 21']
+    browsers: ['Android >= 4', 'iOS >= 7', 'IE >= 9', 'Firefox >= 50', 'Chrome >= 21']
   }
 }
